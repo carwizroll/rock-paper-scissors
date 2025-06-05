293ff9bccfbf482a9b2b4ca9c049e1e73ae811f6
carwizroll <201141617+carwizroll@users.noreply.github.com>
2025-06-05 16:54:21 -0500

    Update .js with random number attempts
    This is likely the wrong direction, but I want a copy of it for reference.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const computerRandomNumber = Math.random() * 100;
+    console.log(computerRandomNumber + "Computer");
+}
+
+function getHumanChoice() {
+    const humanRandomNumber = Math.random() * 100;
+    console.log(humanRandomNumber + "Human");
+}
+
+getComputerChoice();
+getHumanChoice();
