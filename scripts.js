054338da515133048920db41b58f47f7d995af1c
carwizroll <201141617+carwizroll@users.noreply.github.com>
2025-06-06 12:57:37 -0500

    Update JS to generate computer guess
    The computer guess is random

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 function getComputerChoice() {
     const computerRandomNumber = Math.random() * 100;
-    console.log(computerRandomNumber + "Computer");
+    console.log(computerRandomNumber);
+    if (computerRandomNumber < 33 ) {
+    console.log("Rock");
+} else if (computerRandomNumber < 66 ) {
+    console.log("Paper");
+} else {
+    console.log("Scissors");
 }
-
-function getHumanChoice() {
-    const humanRandomNumber = Math.random() * 100;
-    console.log(humanRandomNumber + "Human");
 }
 
 getComputerChoice();
-getHumanChoice();
+
